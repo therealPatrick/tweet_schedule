45fa84b608a7eddf45f2ef33885d0a2abb3f2ef8
therealPatrick <malamashangala13@gmail.com>
2023-12-30 16:54:00 +0200

    sign up errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   get "about", to: "about#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
+  
   #homepage
   root to: "main#index"
 end
