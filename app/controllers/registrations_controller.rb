d13c83b5905c9dd1b2492cbceb77ca9d79bfe083
therealPatrick <malamashangala13@gmail.com>
2023-12-31 11:36:26 +0200

    deleting users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,9 +12,9 @@ class RegistrationsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
-  
-  
+  delete "logout", to: "sessions#destroy"
+
+
   #homepage
   root to: "main#index"
 end
