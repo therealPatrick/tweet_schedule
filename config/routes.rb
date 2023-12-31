fe14a6ddd62a598b011fde735136d18c620ee8d8
therealPatrick <malamashangala13@gmail.com>
2023-12-31 16:04:21 +0200

    edit passwords

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
   get "sign_up", to: "registrations#new"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+
   post "sign_up", to: "registrations#create"
   delete "logout", to: "sessions#destroy"
 
